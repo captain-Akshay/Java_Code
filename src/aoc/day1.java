d46f7682bab8cd6629aa24d25d02b084038b96b4
captain-Akshay <akshay.sharma0503@gmail.com>
2023-09-09 11:18:44 +0530

    day1 part 1 completed

--- src/aoc/day1.java
+++ src/aoc/day1.java
@@ -1,10 +1,21 @@
 package aoc;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class day1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        
+        int ans=0;
+        String i="Input paste it here!!";
+        String[]tempp=i.split("\n\n");
+        for(String st:tempp){
+            int temp=0;
+            for (String input:st.split("\n")){
+                temp+=Integer.valueOf(input);
+            }
+            ans=Math.max(ans,temp);
+        }
+        System.out.println(ans);
     }
 }
